dfacbb628e256a87a09a308284aecfc1d0cd1f95
michaelx0281 <69123340+michaelx0281@users.noreply.github.com>
2025-06-15 21:35:30 -0400

    saving this before anything gets lost

---
+++ src/intro/chapter-1/clumps/main.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"fmt"
+	"io"       //needed to read from (and write to) files
+	"log"      //needed for log files (errors)
+	"net/http" //for accessing URLs
+	"time"
+)
+
+func main() {
+	fmt.Println("Clumps.")
+
+	/*
+		text := "AAAACGTCGAAAA"
+		k := 2
+		L := 4
+		t := 2
+
+		fmt.Println(FindClumps(text, k, L, t))
+	*/
+
+	//rather than copying over an entire genome --> which is going to be so cluttered --> read this from a file from URL
+
+	url := "https://bioinformaticsalgorithms.com/data/realdatasets/Replication/E_coli.txt" // accessing the url will give an response object
+
+	resp, err := http.Get(url) //why is there an REST api cameo....scary
+
+	//error handling
+	if err != nil {
+		panic(err)
+	}
+
+	//reponse was OK
+
+	//close the connection after you're done --> saves to very end of func main
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK { // which is typically 200s
+		log.Fatalf("Received non-OK status: %v", resp.Status)
+	}
+
+	genomeSymbols, err := io.ReadAll(resp.Body) //a slice of all of the symbols read from the body and an error message
+
+	if err != nil {
+		panic(err)
+	}
+
+	//we now have a slice of symbols associated with the genome]
+
+	fmt.Println("The number of nucleotides in E. coli genome is", len(genomeSymbols))
+	//turn slice into string
+	EcoliGenome := string(genomeSymbols)
+
+	k := 9 //the significant number --> frequently
+	L := 500
+	t := 3
+
+	clumps := FindClumps(EcoliGenome, k, L, t)
+
+	fmt.Println("Found", len(clumps), "total patterns occuring as clumps.")
+
+	start := time.Now()
+	FindClumps(EcoliGenome, k, L, t)
+	elapsed := time.Since(start)
+	log.Printf("FindClumps took %s", elapsed)
+
+	start2 := time.Now()
+	clumps2 := FindClumpsFaster(EcoliGenome, k, L, t)
+	elapsed2 := time.Since(start2)
+	log.Printf("FindClumpsFaster took %s", elapsed2)
+
+	fmt.Println("Found", len(clumps2), "total patterns occuring as clumps.")
+}
+
+//text = "BANANASPLIT"
+//first: BANANA
+//second: ANANAS
+
+//FindClumpsFaster takes as input a string text, and integers k, l, and t.
+//It returns a slice of strings representing all substrings of length k that appear at least t times in a "window" of length L in the string text.
+
+// redo this without throwing away the maps fully
+func FindClumpsFaster(text string, k, L, t int) []string {
+	patterns := make([]string, 0)
+	n := len(text)
+
+	//map to track whether I have added a string to patterns yet
+	foundPatterns := make(map[string]bool)
+
+	firstWindow := text[:L]
+
+	freqMap := FrequencyTable(firstWindow, k)
+
+	//append any patterns we find to patterns slice
+	for s, freq := range freqMap {
+		if freq >= t {
+			patterns = append(patterns, s)
+			foundPatterns[s] = true
+		}
+	}
+
+	//range over all remaining possible windows of text
+	for i := 1; i < n-L+1; i++ {
+		//decrease by 1 the value associated with the first substring of length k in the preceding window
+		oldPattern := text[i-1 : i-1+k]
+		freqMap[oldPattern]--
+
+		//clean up the map if the frequency of oldPattern was 1
+		if freqMap[oldPattern] == 0 {
+			delete(freqMap, oldPattern)
+		}
+
+		//add the pattern from the end of the current window
+		newPattern := text[i+L-k : i+L]
+		freqMap[newPattern]++
+
+		//I have updated the frequency map :3
+		//Now I need to update the patterns
+		for s, freq := range freqMap {
+			if freq >= t && !foundPatterns[s] {
+				patterns = append(patterns, s)
+				foundPatterns[s] = true
+			}
+		}
+	}
+
+	return patterns
+}
+
+//FindClumps takes as input a string text, and integers k, l, and t.
+//It returns a slice of strings representing all substrings of length k that appear at least t times in a "window" of length L in the string text.
+
+func FindClumps(text string, k, L, t int) []string {
+	patterns := make([]string, 0)
+	n := len(text)
+
+	//map to track whether I have added a string to patterns yet
+	foundPatterns := make(map[string]bool)
+
+	//range over all possible windows of text --> the windows are of length L
+	for i := 0; i < n-L+1; i++ {
+		//set the current window
+		window := text[i : i+L] // L is bigger than k --> within L look for frequently occuring substrings of length k in L
+
+		//let's make the frequency table for this window
+		freqMap := FrequencyTable(window, k)
+
+		//what occurs frequently(i.e, t or more times)?
+		for s, val := range freqMap {
+			//append s to patterns if  s occurs frequently
+			//and s doesn't already appear in patterns
+			if val >= t && !foundPatterns[s] { //rather than maintaining this as a slice --> map instead --> default value of bool is false
+				patterns = append(patterns, s)
+				foundPatterns[s] = true
+			}
+		}
+	}
+
+	return patterns
+}
+
+//Contains takes as input a slice of strings and a single string s.
+//It returns true if s is contained in the slice and false otherwise.
+
+func Contains(patterns []string, s string) bool {
+	for _, pattern := range patterns {
+		if s == pattern {
+			return true
+		}
+	} // after surviving the ranging --> conclude default and return false
+	return false
+}
+
+//FreqencyTable takes as input a string text and an integer k.
+//It returns the frequency table mapping each substring of text of legnth k to its number of occurences.
+
+func FrequencyTable(text string, k int) map[string]int {
+	freqMap := make(map[string]int) // string is the key, int is the value
+
+	n := len(text)
+
+	//range over all possible substrings of length k --> n - k + 1 total starting positions
+	for i := 0; i < n-k+1; i++ {
+		//grab current pattern
+		pattern := text[i : i+k]
+
+		//updating the value of the freqMap associated with pattern
+
+		//exists --> add one to it
+
+		// _, exists := freqMap[pattern] // exists is a boolean value that could also be returned by the freqMap
+
+		// if !exists {
+		// 	//pattern has not been encounter4ed
+		// 	freqMap[pattern] = 1
+		// } else [
+		// 	//pattern has been encountered +=1
+		// 	freqMap[pattern]++
+		// ]
+
+		// x := freqMap[pattern] // x would would get the default value of keys --> it would get 0
+
+		freqMap[pattern]++ // this is usuable only because the default value of key ints is 0
+		// if pattern is a key, this is what we want
+		//if it's nt, freqMap[pattern] gets created,
+		//gets set equal to zero, then incremented
+
+	}
+
+	return freqMap
+}
---
+++ src/intro/chapter-1/skew/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter" // include line graph support and many other graphs
+	"gonum.org/v1/plot/vg"      // support for vector graphics and image generation
+)
+
+// MinimumSkew
+// Input: a (DNA) string genome
+// Output: a slice of indices corresponding to the indicies in the genome where the skew array att ains a minimum value
+func MinimumSkew(genome string) []int {
+	indices := make([]int, 0)
+
+	//make the skew array
+	array := SkewArray(genome)
+
+	m := MinIntegerArray(array)
+
+	for i, val := range array {
+		if val == m {
+			// I found an index :)
+			indices = append(indices, i)
+		}
+	}
+
+	return indices
+}
+
+func MinIntegerArray(list []int) int {
+	if len(list) == 0 {
+		panic("Error: empty list.")
+	}
+
+	m := list[0]
+
+	for _, val := range list {
+		if val < m {
+			m = val
+		}
+	}
+
+	return m
+}
+
+// SkewArray
+// Input: A DNA string genome
+// Output: A slice of integers corresponding to the "G-C" skew of the genome at each position --> keep track of the difference between the total number of G's and C's in midst of raning the genome!
+func SkewArray(genome string) []int {
+	n := len(genome)
+
+	array := make([]int, n+1)
+
+	array[0] = 0 // redundant but here just for the sake of thinking / pseudocode
+
+	//range over remaining values and set i-th value of array
+
+	for i := 1; i < n+1; i++ {
+		array[i] = array[i-1] + Skew(genome[i-1])
+	}
+
+	return array
+}
+
+//Skew
+//Input: a symbol
+//Output: 1 (if symbol is G), -1 (if symbol is C), and 0 otherwise
+
+func Skew(symbol byte) int {
+	if symbol == 'G' {
+		return 1
+	} else if symbol == 'C' {
+		return -1
+	} else {
+		return 0
+	}
+
+	//default
+	return 0 //biological fact that the circular bacterial genome (plasmic) in 1/2 of the genome before the replication sequence has a depletion of 'G' when compared to 'C' --> after the replication there would be more 'G' than 'C'
+
+	// right now, interested in the position where the skew array goes down and back down, the minimum skew!
+}
+
+func main() {
+	fmt.Println("The skew array.")
+
+	url := "https://bioinformaticsalgorithms.com/data/realdatasets/Replication/E_coli.txt"
+
+	//grab response from site
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close() // close connection later
+
+	//status OK?
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Received non-OK status %v", resp.Status)
+	}
+
+	//access slice of symbols in file
+	genomeSymbols, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//convert genome to string
+
+	genome := string(genomeSymbols)
+	fmt.Println("Genome read. It has", len(genome), "total nucleotides")
+
+	EcoliSkewArray := SkewArray(genome)
+	minSkewPosition := MinimumSkew(genome)
+
+	firstPosition := minSkewPosition[0]
+
+	fmt.Println("The minimum skew value of", EcoliSkewArray[firstPosition], "occurs at positions", minSkewPosition) // The minimum skew value of -13919 occurs at positions [3923620 3923621 3923622 3923623] --> four consecutive positions of the genome!
+	//This is somewhat disappointing --> humans are visual creatures,,, any string has a minimum skew!
+
+	//I am going to make a plot, a Skew Plot
+	//--> get pre-written code for this purpose
+
+}
+
+// MakeSkewDiagram
+// Input: A skew array
+// Output: (none)
+// Draws the skew diagram of the skew array to an image and saves to file.
+func MakeSkewDiagram(skewArray []int) {
+	p := plot.New() // creates a new plotter object
+
+	p.Title.Text = "Skew Diagram"
+	p.X.Label.Text = "Genome Position"
+	p.Y.Label.Text = "Skew value"
+
+	//remove legend
+	p.Legend.Top = false
+
+	// make a collection of points associated with each skew value
+	points := make(plotter.XYs, len(skewArray)) // this is n+1 total points bc the skew array has one extra value
+
+	//set the X and Y value of each point
+	for i, skewValue := range skewArray {
+		points[i].X = float64(i)
+		points[i].Y = float64(skewValue)
+	}
+
+	//connect the dots!
+	line, err := plotter.NewLine(points)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//add our line to the plot
+	p.Add(line)
+
+	//draw to an image
+
+	//first, set a unit of length
+	unitOfLength := vg.Centimeter
+
+	//save my plot to a PNG
+	err = p.Save(100*unitOfLength, 60*unitOfLength, "skewDiagram.png") // w * h
+
+	if err != nil {
+		panic(err)
+	}
+}

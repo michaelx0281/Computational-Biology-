51a219d9a39a8a1b914a5558e5e50ddd542503d6
michaelx0281 <69123340+michaelx0281@users.noreply.github.com>
2025-08-27 15:49:25 -0400

    failed. will circle back on this later if there is time (most practically thinking to restart the whole thing)

--- src/bioinformatics/2/greedyMotif.go
+++ src/bioinformatics/2/greedyMotif.go
@@ -1,8 +1,12 @@
 package main
 
+/*
+	This isn't looking too great at the moment. Skip it if you can and come back to it later. There are more important things in the world :(
+*/
+
 // import "fmt"
 
-import "fmt"
+// import "fmt"
 
 /*
 	This is supposed to exemplify the Greedy Motif Search.
@@ -130,15 +134,18 @@ func GreedyMotifSearch(Dna []string, k, t int) []string {
 		Motif := make([]string, 1)
 		Motif[0] = motif
 
-		for j := 1; j < t; j++ { //there may be an error over here? i am not sure about why the bounds are why they are, but this may need some fixing in the future
+		for j := 1; j <= t-1; j++ { //there may be an error over here? i am not sure about why the bounds are why they are, but this may need some fixing in the future
 			// fmt.Println(Motif)
 			// fmt.Println(Dna[j])
 			profile := ProfileMtx(Motif) //this is really weird and idk if this is the greatest idea
-			fmt.Println(t, j)
 			Motif = append(Motif, ProfileMostProbableKmer(Dna[j], k, profile))
+
+			// if j == t-1 {
+			// 	fmt.Println(Motif)
+			// }
 		}
 
-		if ScoreDistributionMtx(Motif) < ScoreDistributionMtx(BestMotifs) {
+		if ScoreMtx(Motif) < ScoreMtx(BestMotifs) {
 			BestMotifs = Motif
 		}
 	}
@@ -146,31 +153,84 @@ func GreedyMotifSearch(Dna []string, k, t int) []string {
 	return BestMotifs
 }
 
-//entropy is still experimental (aka i don't really want to test or touch it rn) --> lets make a smaller but simpler function creating profiles
+// func ScoreMotif(motif []string) float64 {
+// 	profile := ProfileMtx(motif)
+
+// 	score := 1.0
+
+// 	for i := 0; i < len(profile[0]); i++ {
+// 		max := 0.0
+
+// 		for j := 0; j < 4; j++ {
+// 			if profile[j][i] > max {
+// 				max = profile[j][i]
+// 			}
+// 		}
+
+// 		score *= max
+// 	}
+
+// 	return score
+// }
+
+// //all of the work belew seems to be incorrect, now creating new functions
+
+// //entropy is still experimental (aka i don't really want to test or touch it rn) --> lets make a smaller but simpler function creating profile
 
 //should test next
-func ScoreDistributionMtx(matrix []string) int { //it seems like to me, that I have been doing this wrong.. Need to revise to better fit the problem. Take the largest value out of each col, and multiply them all together
+func ScoreMtx(matrix []string) int { //it seems like to me, that I have been doing this wrong.. Need to revise to better fit the problem. Take the largest value out of each col, and multiply them all together
 	n := len(matrix[0])
+	// t := len(matrix)
 
 	score := 0
 
+	profile := ProfileMtx(matrix)
+
+	dist := ProfileToDist(profile)
+
 	for i := 0; i < n; i++ {
-		score += ScoreCol([]byte{matrix[0][i], matrix[1][i], matrix[2][i], matrix[3][i]})
+		score += ScoreCol([]int{dist[0][i], dist[1][i], dist[2][i], dist[3][i]})
 	}
 
 	return score
 }
 
-func ScoreCol(col []byte) int {
-	//check if all of them are the same
-	if col[0] == col[1] && col[1] == col[2] && col[2] == col[3] {
-		return 0
-	} else {
-		return HammingDist(string(col[0]), string(col[1])) + //this is perhaps one of the ugly-est things i have ever typed out?
-			HammingDist(string(col[1]), string(col[2])) +
-			HammingDist(string(col[2]), string(col[3])) +
-			HammingDist(string(col[3]), string(col[0]))
+func ScoreCol(col []int) int {
+	//find the maxInt amoungest these. Return the total minus the max.
+
+	max := 0
+
+	for i := 0; i < 4; i++ {
+		if col[i] > max {
+			max = col[i]
+		}
 	}
+
+	//by this point we have found the max
+
+	total := col[0] + col[1] + col[2] + col[3]
+
+	return total - max // this is the score at the end
+}
+
+func ProfileToDist(profile [4][]float64) [4][]int {
+	n := len(profile[0])
+	t := len(profile)
+
+	dist := make([][]int, 4)
+
+	for i := range dist {
+		dist[i] = make([]int, n)
+	}
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < n; j++ {
+			profile[i][j] *= float64(t)
+			dist[i][j] = int(profile[i][j])
+		}
+	}
+
+	return [4][]int(dist) //now updated --> this seems like a very dumb approach but I really do not want to start from scratch anymore, this file is also just a giant messs
 }
 
 //TESTED
